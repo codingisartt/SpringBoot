fe088a4654da782c40db764248daebab8afc788f
Tubanur Balci <tubanurbalci@gmail.com>
2022-05-20 19:21:40 +0300

    Shortcuts

---
+++ src/main/java/com/tubanurbalci/springboot/utils/IntellijShortCut.java
@@ -0,0 +1,12 @@
+package com.tubanurbalci.springboot.utils;
+
+public class IntellijShortCut {
+    /*
+    Settings ==> ctrl+alt+s
+    Alt+insert ==> Constructor, toString, getter setter
+    Ctrl+/ ==> Single comment
+    Ctrl+Shift+/ ==> Multiple comment
+
+
+    */
+}
