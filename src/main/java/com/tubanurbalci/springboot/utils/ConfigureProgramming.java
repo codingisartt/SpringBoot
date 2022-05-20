fd99fd46d2b6f47ee7cf7a2e95af8b62c0f7be53
Tubanur Balci <tubanurbalci@gmail.com>
2022-05-20 19:22:10 +0300

    DevTools activate

---
+++ src/main/java/com/tubanurbalci/springboot/utils/ConfigureProgramming.java
@@ -0,0 +1,11 @@
+package com.tubanurbalci.springboot.utils;
+
+public class ConfigureProgramming {
+    /*
+    DevTools ==> LiveReload
+    File ==> Settings ==> Compiler ==> Build project automatically
+    File ==> Advanced Settings ==> Allow Auto make to start even (OK clicked)
+
+
+    */
+}
