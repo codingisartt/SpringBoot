05947866f258a5cad7a3fa3d28798ce906b2849d
Tubanur Balci <tubanurbalci@gmail.com>
2022-05-23 13:07:18 +0300

    Bean (Dto,Config)

--- src/main/java/com/tubanurbalci/springboot/bean/BeanConfig.java
+++ src/main/java/com/tubanurbalci/springboot/bean/BeanConfig.java
@@ -3,10 +3,14 @@ package com.tubanurbalci.springboot.bean;
 import com.tubanurbalci.springboot.dto.BeanDto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BeanConfig {
-    @Bean
+    @Bean(initMethod ="initialBeanMethod",destroyMethod = "destroyBeanMethod")
+    @Scope("singleton")
+//  @Scope("request")
+//  @Scope("session")
     public BeanDto beanDto(){
         return  BeanDto.
                 builder()
--- src/main/java/com/tubanurbalci/springboot/dto/BeanDto.java
+++ src/main/java/com/tubanurbalci/springboot/dto/BeanDto.java
@@ -5,11 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-//@Log4j
+@Log4j2
 @Builder
 public class BeanDto {
 
@@ -17,5 +18,17 @@ public class BeanDto {
     private String beanName;
     private String beanData;
 
+    //initial
+    public void initialBeanMethod(){
+        log.info("Before the Bean starts");
+        System.out.println("Method to run before bean start");
+    }
+
+
+    //end
+    public void destroyBeanMethod(){
+        log.info("After the Bean stop");
+        System.err.println("Method to run after bean finishes");
+    }
 
 }
