72e28b1f0935bd273919c1b288b89dbc7fe0998d
Tubanur Balci <tubanurbalci@gmail.com>
2022-06-03 23:21:41 +0300

    @PathVariable, name, required

--- src/main/java/com/tubanurbalci/springboot/controller/ThymeleafController.java
+++ src/main/java/com/tubanurbalci/springboot/controller/ThymeleafController.java
@@ -4,6 +4,7 @@ import com.tubanurbalci.springboot.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -11,6 +12,12 @@ import java.util.List;
 
 @Controller
 public class ThymeleafController {
+
+    // http://localhost:8080   or http://localhost:8080/index
+    @GetMapping({"/","index"}) //optinal
+    public String index(){
+        return "index";
+    }
 
     // http://localhost:8080/thymeleaf1
     @GetMapping("/thymeleaf1")
@@ -76,4 +83,17 @@ public class ThymeleafController {
         return "thymeleaf6";
     }
 
+    /////////////////////////////////////
+    // Model Object
+    // http://localhost:8080/thymeleaf7/8
+    @GetMapping({"/thymeleaf7","/thymeleaf7/{id}"})
+    public String getThymeleaf7ModelObject(Model model, @PathVariable(name = "id",required = false) Long id){
+        if(id!=null) {
+            model.addAttribute("key_model1", "id: " + id);
+        }else{
+            model.addAttribute("key_model1", "id not found ! ");
+        }
+
+        return "thymeleaf7";
+    }
 }
