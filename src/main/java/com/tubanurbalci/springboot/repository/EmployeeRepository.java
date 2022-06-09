7e3fb1cb189c4ff14da7114840368c576dbd6e73
Tubanur Balci <tubanurbalci@gmail.com>
2022-06-09 23:36:31 +0300

    @Repository(Jpa Hibernate)
    extends JpaRepository
    extends CrudRepository

---
+++ src/main/java/com/tubanurbalci/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.tubanurbalci.springboot.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+//JpaRepository > CrudRepository
+@Repository
+public interface EmployeeRepository extends CrudRepository {
+
+}
