9cab48ecc01c076012546c7dcaf6ec446d134d6c
Tubanur Balci <tubanurbalci@gmail.com>
2022-06-09 19:20:38 +0300

    PasswordEncoderBean (inactive)

---
+++ src/main/java/com/tubanurbalci/springboot/bean/PasswordEncoderBean.java
@@ -0,0 +1,16 @@
+package com.tubanurbalci.springboot.bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//@Configuration
+public class PasswordEncoderBean {
+/*
+    @Bean
+    public PasswordEncoder passwordEncoderBean(){
+        return new BCryptPasswordEncoder();
+    }
+ */
+}
